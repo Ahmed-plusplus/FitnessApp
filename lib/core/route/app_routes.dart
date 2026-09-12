5b2f1cdd26ca03ab83e57ad1dee86b19f069071a
AMostafa <amostafa@zahatechs.com>
2026-09-12 23:47:34 +0300

    chore: add app routes and service locator

---
+++ lib/core/route/app_routes.dart
@@ -0,0 +1,14 @@
+abstract class AppRoutes {
+  static const String splash = '/splash';
+  static const String onboarding = '/onboarding';
+  static const String login = '/login';
+  static const String signup = '/signup';
+  static const String home = '/home';
+  static const String homeBody = '/homeBody';
+  static const String plans = '/plans';
+  static const String planDetails = '/planDetails';
+  static const String trainers = '/trainers';
+  static const String favorites = '/favorites';
+  static const String profile = '/profile';
+  static const String editProfile = '/editProfile';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'core/service/service_locator.dart';
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  setupServiceLocator();
   runApp(const MyApp());
 }
 
