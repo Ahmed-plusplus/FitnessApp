682a80fbd8d1d8742741763dbc594684266c62be
yasmine ibrahim <iayasmine34@gmail.com>
2026-09-13 18:49:56 +0300

    add app colors and theme

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,3 +1,27 @@
-abstract class AppColors {
+import 'package:flutter/material.dart';
 
+class AppColors {
+  AppColors._();
+
+  static const Color primary = Color(0xFF0E80F2);
+  static const Color primaryDark = Color(0xFF0967C7);
+
+  static const Color background = Color(0xFF101922);
+  static const Color surface = Color(0xFF161F2C);
+  static const Color card = Color(0xFF1E293B);
+
+  static const Color textPrimary = Color(0xFFF8FAFC);
+  static const Color textSecondary = Color(0xFF94A3B8);
+  static const Color textMuted = Color(0xFF64748B);
+
+  static const Color border = Color(0xFF263447);
+  static const Color divider = Color(0xFF243041);
+
+  static const Color success = Color(0xFF22C55E);
+  static const Color warning = Color(0xFFF59E0B);
+  static const Color error = Color(0xFFEF4444);
+
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color transparent = Colors.transparent;
 }
