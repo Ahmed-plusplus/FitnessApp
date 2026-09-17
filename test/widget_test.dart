2a409331733575238b0e38824f77435e83b51b74
AMostafa <amostafa@zahatechs.com>
2026-09-18 02:34:45 +0300

    resolve conflicts

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -15,6 +15,6 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const FitnessApp());
 
-    expect(find.byType(MyApp), findsOneWidget);
+    expect(find.byType(FitnessApp), findsOneWidget);
   });
 }
